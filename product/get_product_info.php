1386f454c5e6bc488ad7e77364a4d55fdbad231d
Aliif <mnamz@sfera.com.my>
2023-04-02 20:37:00 +0800

    desc and image style

--- product/get_product_info.php
+++ product/get_product_info.php
@@ -7,7 +7,7 @@ $products = Product::getProduct($id);
 foreach ($products as $product) :
 $html = '<dl class="row">';
 $html .= '<dt class="col-sm-3">Name:</dt><dd class="col-sm-9">' . $product['name'] . '</dd>';
-$html .= '<dt class="col-sm-3">Description:</dt><dd class="col-sm-9">' . $product['description'] . '</dd>';
+$html .= '<dt class="col-sm-3">Description:</dt><dd class="col-sm-9">' . nl2br($product['description']) . '</dd>';
 $html .= '<dt class="col-sm-3">Price:</dt><dd class="col-sm-9">RM ' . $product['price'] . '</dd>';
 $html .= '<img src="uploads/' . $product['image'] . '" class="img-fluid">';
 $html .= '</dl>';
